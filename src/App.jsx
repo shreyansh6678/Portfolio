730ad263162444b42c45e6f17f8e629dc0d992c5
shreyansh6678 <shreyanshsharma678@gmail.com>
2026-05-27 17:17:35 +0530

    cursor updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,19 @@ import Skillspage from './components/skillpage/skillspage'
 import ProjectsPage from './components/projectpage/projects'
 import Resume from './components/resumepage/resume'
 import Contact from './components/contactpage/getintouch'
+import AnimatedCursor from "react-animated-cursor";
 function App() {
 
   return (
    <div>
+     <AnimatedCursor
+        innerSize={10}
+        outerSize={35}
+        color="86, 204, 242"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={2}
+      />
    <Firstpage/>
    <Secondpage/>
    <Skillspage/>
